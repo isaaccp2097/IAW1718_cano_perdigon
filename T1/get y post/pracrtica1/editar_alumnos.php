43d8f2e311526be0a66925ebd74d52aa8402f450
Your Name <you@example.com>
2017-11-09 09:06:58 +0100

    practica editar alumnos terminada

--- T1/get y post/pracrtica1/editar_alumnos.php
+++ T1/get y post/pracrtica1/editar_alumnos.php
@@ -6,22 +6,37 @@
   <body>
 
 
-    <form action="editar_alumnos.php" method="post">
+    <form method="post">
       <fieldset>
         <legend>Editar alumnos</legend>
 
 
      <?php
-        if(isset($_GET["nombre"])) {
+        if(isset($_GET["nombre"]) && !isset($_POST["nombre"])) {
           $var=($_GET["nombre"]);
           echo "<span>Nombre :</span><input type='text' name='nombre' value='$var' required><br>";
           $v=($_GET["apellido"]);
           echo "<span>Apellido :</span><input type='text' name='apellido' value='$v' required><br>";
           $v=($_GET["correo"]);
-          echo "<span>Correo :</span><input type='mail' name='correo' value='$v' required><br>";
+          echo "<span>Correo :</span><input type='email' name='correo' value='$v' required><br>";
+          echo "<p><input type='submit' value='Editar' ></p>";
+        }
+
+        else {
+          if (empty($_GET)) {
+            echo "No tenemos ningun dato para editar";
+          }
+          else {
+            echo "<h1>Datos actualizados</h1>";
+            echo "<b>Nombre: </b>".$_POST["nombre"]."<br>";
+            echo "<b>Apellido: </b>".$_POST["apellido"]."<br>";
+            echo "<b>Correo: </b>".$_POST["correo"]."<br>";
+            echo "<a href='tablaalumnos.php'>Volver</a>";
+          }
+
         }
      ?>
-        <p><input type="submit" value="Send" ></p>
+
       </fieldset>
     </form>
 
